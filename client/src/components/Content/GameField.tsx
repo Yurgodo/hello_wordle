f093eba81fe90c33a5d42263eb62cb01dbb6bc36
godorov <godorov@greendatasoft.ru>
2022-01-25 01:04:40 +0500

    start of game logic

--- client/src/components/Content/GameField.tsx
+++ client/src/components/Content/GameField.tsx
@@ -1,8 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
+import {useAppStore} from "../../store/store";
+
+const gameFieldContent = (colCount: number, rowCount: number, wordState: string, gameState: Array<string>) => {
+    const resultArr = [];
+    for (let i = 1; i <= colCount; i++) {
+        let wordComponent = (
+            <Word size={colCount} wordState={gameState[i] || ""}/>
+        )
+        if (i === gameState.length + 1) {
+            wordComponent = (
+                <Word wordState={wordState} size={colCount}/>
+            )
+        }
+        resultArr.push(wordComponent)
+    }
+    return resultArr.map(component => component)
+}
 
 const GameField = () => {
+    const [wordState, setWordState] = useState("")
+    const {gameState, currentWord} = useAppStore()
+    const colCount = currentWord.length
+    const rowCount = colCount + 1
+
     return (
         <React.Fragment>
+            {gameFieldContent(colCount, rowCount, wordState, gameState)}
         </React.Fragment>
     )
 }
